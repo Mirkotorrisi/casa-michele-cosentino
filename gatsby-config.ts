e67faf2394b41b0d55ea238c2f25204319e0fc81
mirkotorrisi <mirko.torrisi92@gmail.com>
2024-01-12 09:20:36 +0100

    fixed seo issues

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,6 +4,9 @@ const config: GatsbyConfig = {
   siteMetadata: {
     title: `Casa di cura Michele Cosentino`,
     siteUrl: `https://www.yourdomain.tld`,
+    lang: "it-IT",
+    description:
+      "Casa Mons.Michele Cosentino, la residenza che accoglie anziani, alla ricerca di un contesto familiare nel quale vivere serenamente la propria quotidianità",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
